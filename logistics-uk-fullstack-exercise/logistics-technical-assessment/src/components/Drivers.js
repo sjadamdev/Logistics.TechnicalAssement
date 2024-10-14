3a76b9391cf7c438bdd0e46b04096c0a774e97f6
Adam Smith <sjadamdev@gmail.com>
2024-10-14 23:47:07 +0100

    Driver list

--- logistics-uk-fullstack-exercise/logistics-technical-assessment/src/components/Drivers.js
+++ logistics-uk-fullstack-exercise/logistics-technical-assessment/src/components/Drivers.js
@@ -1,11 +1,54 @@
+import React,{useState,useEffect} from 'react';
+
 function Drivers() {
+
+    const [drivers,setDrivers]=useState([]);
+    
+    const getDrivers=()=>{
+        fetch('/data/drivers.json'
+        ,{
+          headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+           }
+        }
+        )
+          .then(function(response){
+            console.log(response)
+            return response.json();
+          })
+          .then(function(myJson) {
+            console.log(myJson);
+            setDrivers(myJson.data)
+          });
+      }
+      
+      useEffect(()=>{
+        getDrivers()
+      },[])
+
     return (
         <div style={{ backgroundColor: 'lightblue', color: 'white', padding: '15px', flex: 1, overflow: 'auto'}}>
             <main className="main-content">
-                <h2>Welcome to the Main Content Section</h2>
-                <p>This is the section where the main content will be displayed.</p>
+              <table style={{ textAlign: 'left'}}>
+                <thead>
+                  <tr>
+                    <th>Driver Name</th>
+                    <th>Vehicle Reg</th>
+                  </tr>
+                </thead>
+                <tbody>
+                {drivers.map((driver, index) => (
+                  <tr>
+                    <td>{driver.forename} {driver.surname}</td>
+                    <td>{driver.vehicleRegistration}</td>
+                  </tr>
+                ))}
+                </tbody>
+              </table>
             </main>
         </div>
+
     )
 };
 
